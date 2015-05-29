8c87ff0f8bdda7f8d3e29a2bd435a9066b26b1b4
Jeff Hou <j.king.hou@gmail.com>
2015-05-29 10:03:45 -0400

    Added edit functionality.

--- hello.py
+++ hello.py
@@ -8,8 +8,24 @@ bookmarks = []
 def hello():
     return render_template('index.html', bookmarks=bookmarks, page_id=page_id)
 
+@app.route("/<int:page_id>/edit_bookmark/<int:bookmark_id>/", methods=['POST'])
+def edit_bookmark(page_id, bookmark_id):
+    print(request.form)
+    for i in request.form:
+        print(i, request.form[i])
+    if request.method == 'POST':
+        new_bm = {}
+        new_bm['name'] = request.form['name']
+        new_bm['url'] = request.form['link']
+        new_bm['page_id'] = page_id
+        bookmarks[bookmark_id] = new_bm
+    return redirect(url_for('hello'))
+
 @app.route("/<int:page_id>/add_bookmark/", methods=['POST'])
 def add_bookmark(page_id):
+    print(request.form)
+    for i in request.form:
+        print(i, request.form[i])
     if request.method == 'POST':
         new_bm = {}
         new_bm['name'] = request.form['name']
